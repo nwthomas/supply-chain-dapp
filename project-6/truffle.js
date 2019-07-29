7f4bc1539e44751bb82f33ad4b4490194c5498d2
Nathan Thomas <nwthomas@me.com>
2019-07-28 22:57:53 -0700

    Add beginning project write up

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,59 @@
+require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Matches any network ID
+    },
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          "https://ropsten.infura.io/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    kovan: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 42,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    main: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY
+        ),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
     }
   }
 };
---
+++ project-write-up.md
@@ -0,0 +1,21 @@
+# PROJECT WRITE_UP
+
+## TABLE OF CONTENTS
+
+- [Unified Modeling Language](#unified-modeling-language)
+- [Libraries](#libraries)
+- [IPFS](#ipfs)
+
+## UNIFIED MODELING LANGUAGE
+
+The following images represent a brief overview of the processes, modeling, and state of the DApp. First, here is an example of the sequence diagram for the coffee cycle:
+
+![Sequence diagram for coffee supply chain management](./images/coffeeSequenceDiagram.png)
+
+![State diagram for coffee supply management](./images/stateDiagram.png)
+
+![Data modeling diagram for coffee supply chain management](./images/dataModeling.png)
+
+## LIBRARIES
+
+## IPFS
